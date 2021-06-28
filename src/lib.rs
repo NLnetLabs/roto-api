c889900719b68d0afad21cf120531745ba4a4eab
Jasper den Hertog <jasper@plainspace.com>
2021-06-28 21:21:21 +0200

    JsonBuilder + Display impl for members

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,15 @@
-use std::{fmt, slice};
+use ansi_term::Colour;
+use chrono::{DateTime, Utc};
+use num::PrimInt;
+use rotonda_store::common::{AddressFamily, MergeUpdate, Prefix as RotondaPrefix};
+use rotonda_store::{InMemNodeId, InMemStorage, SizedStrideNode, TreeBitMap};
 use std::error::Error;
+use std::fmt::Write;
 use std::fs::File;
 use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
 use std::path::Path;
 use std::str::FromStr;
-use ansi_term::Colour;
-use chrono::{DateTime, Utc};
-use num::PrimInt;
-use rotonda_store::common::{
-    AddressFamily, MergeUpdate,
-    Prefix as RotondaPrefix
-};
-use rotonda_store::{InMemNodeId, InMemStorage, SizedStrideNode, TreeBitMap};
-
+use std::{fmt, slice};
 
 //------------ Addr ----------------------------------------------------------
 
@@ -54,19 +51,18 @@ impl FromStr for Addr {
 impl fmt::Display for Addr {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
-            Addr::V4(addr) => addr.fmt(f),
-            Addr::V6(addr) => addr.fmt(f),
+            Addr::V4(addr) => write!(f, "{}", std::net::Ipv4Addr::from(*addr)),
+            Addr::V6(addr) => write!(f, "{}", std::net::Ipv6Addr::from(*addr)),
         }
     }
 }
-
 
 //------------ Prefix --------------------------------------------------------
 
 #[derive(Clone, Copy, Debug)]
 pub struct Prefix {
     pub addr: Addr,
-    pub len: u8
+    pub len: u8,
 }
 
 impl Prefix {
@@ -75,6 +71,11 @@ impl Prefix {
     }
 }
 
+impl fmt::Display for Prefix {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}/{}", self.addr, self.len)
+    }
+}
 
 //------------ RecordSet -----------------------------------------------------
 
@@ -93,15 +94,13 @@ impl<'a> RecordSet<'a> {
         RecordSetIter {
             v4: if self.v4.is_empty() {
                 None
-            }
-            else {
+            } else {
                 Some(self.v4.iter())
             },
             v6: self.v6.iter(),
         }
     }
 }
-
 
 //------------ RecordSetIter -------------------------------------------------
 
@@ -119,16 +118,22 @@ impl<'a, 'b> Iterator for RecordSetIter<'a, 'b> {
         if self.v4.is_none() {
             return self.v6.next().map(|res| {
                 (
-                    Prefix { addr: Addr::V6(res.net), len: res.len },
-                    res.meta.as_ref()
+                    Prefix {
+                        addr: Addr::V6(res.net),
+                        len: res.len,
+                    },
+                    res.meta.as_ref(),
                 )
-            })
+            });
         }
 
         if let Some(res) = self.v4.as_mut().and_then(|v4| v4.next()) {
             return Some((
-                Prefix { addr: Addr::V4(res.net), len: res.len },
-                res.meta.as_ref()
+                Prefix {
+                    addr: Addr::V4(res.net),
+                    len: res.len,
+                },
+                res.meta.as_ref(),
             ));
         }
         self.v4 = None;
@@ -136,11 +141,10 @@ impl<'a, 'b> Iterator for RecordSetIter<'a, 'b> {
     }
 }
 
-
 //------------ Rir -----------------------------------------------------------
 
 #[derive(Clone, Copy, Debug)]
-enum Rir {
+pub enum Rir {
     Afrinic,
     Apnic,
     Arin,
@@ -149,7 +153,7 @@ enum Rir {
     Unknown,
 }
 
-impl<'a> From<&'a str> for  Rir {
+impl<'a> From<&'a str> for Rir {
     fn from(str: &str) -> Self {
         match str {
             "afrinic" => Self::Afrinic,
@@ -162,14 +166,23 @@ impl<'a> From<&'a str> for  Rir {
     }
 }
 
+impl<'a> fmt::Display for Rir {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Rir::Afrinic => write!(f, "AFRINIC"),
+            Rir::Apnic => write!(f, "APNIC"),
+            Rir::Arin => write!(f, "ARIN"),
+            Rir::Lacnic => write!(f, "LACNIC"),
+            Rir::RipeNcc => write!(f, "RIPE NCC"),
+            Rir::Unknown => write!(f, "Unknown"),
+        }
+    }
+}
 
 //------------ ExtPrefixRecord -----------------------------------------------
 
 #[derive(Clone, Debug, Default)]
-pub struct ExtPrefixRecord(
-    pub Option<RirDelExtRecord>,
-    pub Option<RisWhoisRecord>
-);
+pub struct ExtPrefixRecord(pub Option<RirDelExtRecord>, pub Option<RisWhoisRecord>);
 
 impl MergeUpdate for ExtPrefixRecord {
     fn merge_update(
@@ -181,7 +194,19 @@ impl MergeUpdate for ExtPrefixRecord {
         }
 
         if update_record.1.is_some() {
-            self.1 = update_record.1
+            match &mut self.1 {
+                Some(ris_whois_rec) => {
+                    if let Some(update_ris_rec) = update_record.1 {
+                        ris_whois_rec
+                            .origin_as
+                            .0
+                            .push(update_ris_rec.origin_as.0[0]);
+                    }
+                }
+                None => {
+                    self.1 = update_record.1;
+                }
+            }
         }
 
         Ok(())
@@ -191,12 +216,45 @@ impl MergeUpdate for ExtPrefixRecord {
 #[derive(Clone, Debug)]
 pub struct RirDelExtRecord {
     group_id: String,
-    rir: Rir,
+    pub rir: Rir,
 }
 
 #[derive(Clone, Debug)]
+pub struct AsnArray(pub Vec<Asn>);
+
+#[derive(Clone, Debug)]
 pub struct RisWhoisRecord {
-    origin_as: Vec<u32>,
+    pub origin_as: AsnArray,
+}
+
+impl fmt::Display for AsnArray {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let arr_str = self.0.iter().fold("".to_string(), |as_arr, asn| {
+            let asn_str: &str = &asn.to_string();
+            as_arr + "AS" + asn_str
+        });
+        write!(f, "{}", arr_str)
+    }
+}
+
+// Not really used right now, since the
+// impl Display isn't used either. May make sense
+// to redefine Asn to be an enum that can either
+// be a u32 or a PRIVATE_ASN.
+#[derive(Copy, Clone, Debug)]
+pub struct Asn(u32);
+
+// Unused
+impl fmt::Display for Asn {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "AS{},", self.0)
+    }
+}
+
+impl Asn {
+    fn from_str(as_str: &str) -> Result<Asn, std::num::ParseIntError> {
+        as_str.parse::<u32>().map(|asn| Asn(asn))
+    }
 }
 
 //------------ Store ---------------------------------------------------------
@@ -206,7 +264,6 @@ pub struct Store {
     v6: TreeBitMap<InMemStorage<u128, ExtPrefixRecord>>,
     updated: DateTime<Utc>,
 }
-
 
 impl Store {
     pub fn new() -> Self {
@@ -221,43 +278,56 @@ impl Store {
         self.updated
     }
 
-    pub fn load_riswhois(
-        &mut self,
-        path: &Path,
-    ) -> Result<(), Box<dyn Error>> {
+    pub fn load_riswhois(&mut self, path: &Path) -> Result<(), Box<dyn Error>> {
         let file = File::open(path)?;
         let mut rdr = csv::Reader::from_reader(file);
         for result in rdr.records() {
             let record = result?;
-            let net = Addr::from_str(&record[0])?;
-            let len = u8::from_str(&record[1])?;
-            let asn = u32::from_str(&record[2])?;
+            let net = Addr::from_str(&record[0]).unwrap_or_else(|_| {
+                println!("Error parsing {}/{}", &record[0], &record[1]);
+                panic!("can't continue parsing")
+            });
+            let len = u8::from_str(&record[1]).unwrap_or_else(|_| {
+                println!("Error parsing {}/{}", &record[0], &record[1]);
+                panic!("can't continue parsing")
+            });
+            let asn: Asn = Asn::from_str(&record[2]).unwrap_or_else(|e| {
+                println!("{:?}", e);
+                println!(
+                    "Error parsing {}/{} with asn AS{}",
+                    &record[0], &record[1], &record[2]
+                );
+                panic!("can't continue parsing")
+            });
             let meta = ExtPrefixRecord(
                 None,
-                Some(RisWhoisRecord { origin_as: vec![asn] })
+                Some(RisWhoisRecord {
+                    origin_as: AsnArray(vec![asn]),
+                }),
             );
 
             match net {
-                Addr::V4(net) => {
-                    self.v4.insert(
-                        RotondaPrefix::new_with_meta(net, len, meta)
-                    )?
-                }
-                Addr::V6(net) => {
-                    self.v6.insert(
-                        RotondaPrefix::new_with_meta(net, len, meta)
-                    )?
-                }
+                Addr::V4(net) => self
+                    .v4
+                    .insert(RotondaPrefix::new_with_meta(net, len, meta))
+                    .unwrap_or_else(|_| {
+                        println!("Error parsing {} {}", net, len);
+                        panic!("can't continue parsing")
+                    }),
+                Addr::V6(net) => self
+                    .v6
+                    .insert(RotondaPrefix::new_with_meta(net, len, meta))
+                    .unwrap_or_else(|_| {
+                        println!("Error parsing {} {}", net, len);
+                        panic!("can't continue parsing")
+                    }),
             }
         }
         self.updated = Utc::now();
         Ok(())
     }
 
-    pub fn load_prefixes(
-        &mut self,
-        path: &Path,
-    ) -> Result<(), Box<dyn Error>> {
+    pub fn load_prefixes(&mut self, path: &Path) -> Result<(), Box<dyn Error>> {
         let file = File::open(path)?;
         let mut rdr = csv::ReaderBuilder::new()
             .delimiter(b'|')
@@ -273,17 +343,18 @@ impl Store {
                 || &record[6] == "reserved"
                 || &record[6] == "available"
             {
-                continue
+                continue;
             }
 
             let group_id = match record.get(7) {
                 Some(id) => id.to_string(),
-                None => continue
+                None => continue,
             };
 
             let meta = ExtPrefixRecord(
                 Some(RirDelExtRecord {
-                    group_id, rir: record[0].into(),
+                    group_id,
+                    rir: record[0].into(),
                 }),
                 None,
             );
@@ -298,20 +369,18 @@ impl Store {
                     let len_base = u32::from_str(&record[4])?;
                     let len: u8 = (len_base.leading_zeros() + 1) as u8;
 
-                    self.v4.insert(
-                        RotondaPrefix::new_with_meta(net.into(), len, meta)
-                    )?;
+                    self.v4
+                        .insert(RotondaPrefix::new_with_meta(net.into(), len, meta))?;
                 }
                 "ipv6" => {
                     let net = Ipv6Addr::from_str(&record[3])?;
-                    
+
                     // record[4] is just the prefix length here. No shenanigans
                     // necessary.
                     let len = u8::from_str(&record[4])?;
 
-                    self.v6.insert(
-                        RotondaPrefix::new_with_meta(net.into(), len, meta)
-                    )?;
+                    self.v6
+                        .insert(RotondaPrefix::new_with_meta(net.into(), len, meta))?;
                 }
                 _ => {}
             }
@@ -322,29 +391,22 @@ impl Store {
 
     pub fn match_longest_prefix(&self, prefix: Prefix) -> RecordSet {
         match prefix.addr {
-            Addr::V4(addr) => {
-                RecordSet {
-                    v4: self.v4.match_longest_prefix(
-                        &RotondaPrefix::new(addr, prefix.len)
-                    ),
-                    v6: Vec::new(),
-                }
-            }
-            Addr::V6(addr) => {
-                RecordSet {
-                    v4: Vec::new(),
-                    v6: self.v6.match_longest_prefix(
-                        &RotondaPrefix::new(addr, prefix.len)
-                    )
-                }
-            }
+            Addr::V4(addr) => RecordSet {
+                v4: self
+                    .v4
+                    .match_longest_prefix(&RotondaPrefix::new(addr, prefix.len)),
+                v6: Vec::new(),
+            },
+            Addr::V6(addr) => RecordSet {
+                v4: Vec::new(),
+                v6: self
+                    .v6
+                    .match_longest_prefix(&RotondaPrefix::new(addr, prefix.len)),
+            },
         }
     }
 
-    pub fn get_related_prefixes(
-        &self,
-        meta: &RirDelExtRecord
-    ) -> RecordSet {
+    pub fn get_related_prefixes(&self, meta: &RirDelExtRecord) -> RecordSet {
         RecordSet {
             v4: Self::_get_related_prefixes(&self.v4, meta),
             v6: Self::_get_related_prefixes(&self.v6, meta),
@@ -353,26 +415,23 @@ impl Store {
 
     fn _get_related_prefixes<'b, T: AddressFamily>(
         tree: &'b TreeBitMap<InMemStorage<T, ExtPrefixRecord>>,
-        meta: &RirDelExtRecord
+        meta: &RirDelExtRecord,
     ) -> Vec<&'b RotondaPrefix<T, ExtPrefixRecord>> {
-        tree.store.prefixes
-        .iter()
-        .filter(|&rel_p| {
-            if let Some(rel_p_meta) = rel_p.meta.as_ref() {
-                if let Some(rel_p_meta_rde)
-                    = rel_p_meta.0.as_ref()
-                {
-                    rel_p_meta_rde.group_id == meta.group_id
-                }
-                else {
+        tree.store
+            .prefixes
+            .iter()
+            .filter(|&rel_p| {
+                if let Some(rel_p_meta) = rel_p.meta.as_ref() {
+                    if let Some(rel_p_meta_rde) = rel_p_meta.0.as_ref() {
+                        rel_p_meta_rde.group_id == meta.group_id
+                    } else {
+                        return false;
+                    }
+                } else {
                     return false;
                 }
-            }
-            else {
-                return false;
-            }
-        })
-        .collect()
+            })
+            .collect()
     }
 
     pub fn output_stats(&self) {
@@ -383,7 +442,7 @@ impl Store {
     }
 
     fn output_tree_stats<AF: AddressFamily + PrimInt + fmt::Debug>(
-        tree_bitmap: &TreeBitMap<InMemStorage<AF, ExtPrefixRecord>>
+        tree_bitmap: &TreeBitMap<InMemStorage<AF, ExtPrefixRecord>>,
     ) {
         let total_nodes = tree_bitmap.stats.iter().fold(0, |mut acc, c| {
             acc += c.created_nodes.iter().fold(0, |mut sum, l| {
@@ -410,7 +469,7 @@ impl Store {
         println!(
             "memory used by prefixes: {}kb",
             tree_bitmap.store.prefixes.len()
-            * std::mem::size_of::<RotondaPrefix<AF, ExtPrefixRecord>>()
+                * std::mem::size_of::<RotondaPrefix<AF, ExtPrefixRecord>>()
                 / 1024
         );
         println!("stride division  {:?}", tree_bitmap.strides);
@@ -486,4 +545,189 @@ impl Store {
     }
 }
 
+//------------ JsonBuilder ---------------------------------------------------
 
+/// A helper type for building a JSON-encoded string on the fly.
+///
+/// Note that the builder only supports strings without control characters.
+pub struct JsonBuilder<'a> {
+    target: &'a mut String,
+    indent: usize,
+    empty: bool,
+}
+
+impl JsonBuilder<'static> {
+    pub fn build<F: FnOnce(&mut JsonBuilder)>(op: F) -> String {
+        let mut target = String::new();
+        JsonBuilder {
+            target: &mut target,
+            indent: 0,
+            empty: true,
+        }
+        .array_object(op);
+        target
+    }
+}
+
+impl<'a> JsonBuilder<'a> {
+    pub fn member_object<F: FnOnce(&mut JsonBuilder)>(&mut self, key: impl fmt::Display, op: F) {
+        self.append_key(key);
+        self.target.push_str("{\n");
+        op(&mut JsonBuilder {
+            target: self.target,
+            indent: self.indent + 1,
+            empty: true,
+        });
+        self.target.push('\n');
+        self.append_indent();
+        self.target.push('}');
+    }
+
+    pub fn member_array<F: FnOnce(&mut JsonBuilder)>(&mut self, key: impl fmt::Display, op: F) {
+        self.append_key(key);
+        self.target.push_str("[\n");
+        op(&mut JsonBuilder {
+            target: self.target,
+            indent: self.indent + 1,
+            empty: true,
+        });
+        self.target.push('\n');
+        self.append_indent();
+        self.target.push(']');
+    }
+
+    pub fn member_str(&mut self, key: impl fmt::Display, value: impl fmt::Display) {
+        self.append_key(key);
+        self.target.push('"');
+        write!(
+            JsonString {
+                target: self.target
+            },
+            "{}",
+            value
+        )
+        .unwrap();
+        self.target.push('"');
+    }
+
+    pub fn member_raw(&mut self, key: impl fmt::Display, value: impl fmt::Display) {
+        self.append_key(key);
+        write!(
+            JsonString {
+                target: self.target
+            },
+            "{}",
+            value
+        )
+        .unwrap();
+    }
+
+    pub fn array_object<F: FnOnce(&mut JsonBuilder)>(&mut self, op: F) {
+        self.append_array_head();
+        self.append_indent();
+        self.target.push_str("{\n");
+        op(&mut JsonBuilder {
+            target: self.target,
+            indent: self.indent + 1,
+            empty: true,
+        });
+        self.target.push('\n');
+        self.append_indent();
+        self.target.push('}');
+    }
+
+    pub fn array_array<F: FnOnce(&mut JsonBuilder)>(&mut self, op: F) {
+        self.append_array_head();
+        self.append_indent();
+        self.target.push_str("[\n");
+        op(&mut JsonBuilder {
+            target: self.target,
+            indent: self.indent + 1,
+            empty: true,
+        });
+        self.target.push('\n');
+        self.append_indent();
+        self.target.push(']');
+    }
+
+    pub fn array_str(&mut self, value: impl fmt::Display) {
+        self.append_array_head();
+        self.append_indent();
+        self.target.push('"');
+        write!(
+            JsonString {
+                target: self.target
+            },
+            "{}",
+            value
+        )
+        .unwrap();
+        self.target.push('"');
+    }
+
+    pub fn array_raw(&mut self, value: impl fmt::Display) {
+        self.append_array_head();
+        self.append_indent();
+        write!(
+            JsonString {
+                target: self.target
+            },
+            "{}",
+            value
+        )
+        .unwrap();
+    }
+
+    fn append_key(&mut self, key: impl fmt::Display) {
+        if self.empty {
+            self.empty = false
+        } else {
+            self.target.push_str(",\n");
+        }
+        self.append_indent();
+        self.target.push('"');
+        write!(
+            JsonString {
+                target: self.target
+            },
+            "{}",
+            key
+        )
+        .unwrap();
+        self.target.push('"');
+        self.target.push_str(": ");
+    }
+
+    fn append_array_head(&mut self) {
+        if self.empty {
+            self.empty = false
+        } else {
+            self.target.push_str(",\n");
+        }
+    }
+
+    fn append_indent(&mut self) {
+        for _ in 0..self.indent {
+            self.target.push_str("   ");
+        }
+    }
+}
+
+//------------ JsonString ----------------------------------------------------
+
+struct JsonString<'a> {
+    target: &'a mut String,
+}
+
+impl<'a> fmt::Write for JsonString<'a> {
+    fn write_str(&mut self, mut s: &str) -> Result<(), fmt::Error> {
+        while let Some(idx) = s.find(|ch| ch == '"' || ch == '\\') {
+            self.target.push_str(&s[..idx]);
+            self.target.push('\\');
+            self.target.push(char::from(s.as_bytes()[idx]));
+            s = &s[idx + 1..];
+        }
+        self.target.push_str(s);
+        Ok(())
+    }
+}
