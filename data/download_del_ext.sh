0f08cc34419926490d35fc890c69c96c969c7bf4
Jasper den Hertog <jasper@plainspace.com>
2021-06-29 13:21:33 +0000

    split downloads

--- data/download_del_ext.sh
+++ data/download_del_ext.sh
@@ -21,14 +21,3 @@ cat delegated-apnic-extended-latest.txt >> delegated-all && \
 cat delegated-arin-extended-latest.txt >> delegated-all && \
 cat delegated-lacnic-extended-latest.txt >> delegated-all && \
 cat delegated-ripencc-extended-latest.txt >> delegated-all
-
-echo RISWHois IPv4
-curl -o riswhoisdump-ipv4.gz http://www.ris.ripe.net/dumps/riswhoisdump.IPv4.gz
-
-echo RISWhois IPv6
-curl -o riswhoisdump-ipv6.gz http://www.ris.ripe.net/dumps/riswhoisdump.IPv6.gz
-
-echo Refactor RISWhois
-rg -e '(\d+)\t(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})/(\d+)\t(\d+)' -N --replace '$2,$3,$1' data/riswhoisdump.IPv4 > pfx_asn_dfz_v4.csv
-rg -e '(\d+)\t([0-9abcdef:]+)/(\d{1,3})\t(\d+)$' -N --replace '$2,$3,$1' riswhoisdump.IPv6 > pfx_asn_dfz_v6.csv
-cat pfx_asn_dfz_v4.csv > pfx_asn-all.csv && cat pfx_asn_dfz_v6.csv >> pfx_asn-all.csv
--- data/download_ris_whois.sh
+++ data/download_ris_whois.sh
@@ -11,4 +11,3 @@ gunzip riswhoisdump-ipv6.gz
 echo Refactor RISWhois
 rg -e '(\d+)\t(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})/(\d+)\t(\d+)' -N --replace '$2,$3,$1' riswhoisdump-ipv4 > pfx_asn_dfz_v4.csv
 rg -e '(\d+)\t([0-9abcdef:]+)/(\d{1,3})\t(\d+)$' -N --replace '$2,$3,$1' riswhoisdump-ipv6 > pfx_asn_dfz_v6.csv
-cat pfx_asn_dfz_v4.csv > pfx_asn-all.csv && cat pfx_asn_dfz_v6.csv >> pfx_asn-all.csv
