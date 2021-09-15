113cbcb154558844fb93737555339b21de913c1e
Jasper den Hertog <jasper@plainspace.com>
2021-09-15 09:28:38 +0200

    Add members field to empty response

--- src/bin/api.rs
+++ src/bin/api.rs
@@ -97,7 +97,8 @@ pub fn match_prefix_output(store: &Store, prefix: Prefix, match_options: MatchOp
             } else {
                 builder.member_raw("prefix", "null");
                 builder.member_str("type", &recs.match_type);
-                builder.member_raw("meta", "[]");
+                builder.member_raw("meta", "[]]");
+                builder.member_raw("members", "[]");
             }
 
             // See whether the result_prefix has a DelExtRecord, if it does
